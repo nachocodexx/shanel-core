0db3517e8804b58e5e48d2ef1fb2d95e1a1a4896
Nacho Castillo <ignacio.bcastillo@gmail.com>
2022-08-15 22:53:40 -0500

    Stable version [:fire:]

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Needed for dependencies
     install_requires=['numpy'],
     # *strongly* suggested for sharing
-    version='0.10',
+    version='0.11',
     # The license can be anything you like
     license='MIT',
     description='An example of a python package from pre-existing code',
