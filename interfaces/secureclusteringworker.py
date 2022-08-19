272ae9673400f41671da710d5b21733f1cd4695f
Nacho Castillo <ignacio.bcastillo@gmail.com>
2022-08-18 21:03:10 -0500

    Stable version [:fire:]

--- interfaces/secureclusteringworker.py
+++ interfaces/secureclusteringworker.py
@@ -25,16 +25,18 @@ class SecureClusteringWorker(object):
     # { a: 1 } + {b: 2} = {a:1,b:2}
     # { **{a:1},**{b:2,c:3} } = {a:1,b:2,c:3}
     def SKMeans_1(self,**kwargs) -> Response:
+        timeout = kwargs.pop("timeout",None)
         return self.session.post(
             "http://{}:{}/clustering/skmeans/1".format(self.workerId,self.port),
              headers = kwargs,
-             timeout = 100
+             timeout = timeout
         )
 
     def SKMeans_2(self,**kwargs) -> Response:
+        timeout = kwargs.pop("timeout",None)
         return self.session.post(
             "http://{}:{}/clustering/skmeans/2".format(self.workerId,self.port),
              headers = kwargs,
-             timeout = 100
+             timeout = timeout
         )
         # self.
