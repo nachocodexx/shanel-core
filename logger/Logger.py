d4af7804ffa829dd4d64cb7ba8b608e77d627f18
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2022-10-15 21:09:47 -0500

    Logger [fixed]

--- logger/Logger.py
+++ logger/Logger.py
@@ -2,15 +2,16 @@ import sys
 import logging
 
 def create_logger(**kwargs):
-    name        = kwargs.get("name","default")
-    LOG_PATH    = kwargs.get("LOG_PATH")
-    NODE_ID     = kwargs.get("NODE_ID")
+    name          = kwargs.get("name","default")
+    LOG_PATH      = kwargs.get("LOG_PATH")
+    LOG_FILENAME  = kwargs.get("LOG_FILENAME")
+    add_error_log = kwargs.get("add_error_log",True)
     # 
     FORMAT      = '%(asctime)s,%(msecs)s,%(levelname)s,%(threadName)s,%(message)s'
     formatter   = logging.Formatter(FORMAT,"%Y-%m-%d,%H:%M:%S")
     # 
-    filename      = "{}/{}.csv".format(LOG_PATH,NODE_ID)
-    errorFilename = "{}/{}-error.log".format(LOG_PATH,NODE_ID)
+    filename      = "{}/{}.csv".format(LOG_PATH,LOG_FILENAME)
+    errorFilename = "{}/{}-error.log".format(LOG_PATH,LOG_FILENAME)
     # 
     # 
     logger      = logging.getLogger(name)
@@ -24,14 +25,15 @@ def create_logger(**kwargs):
     filehandler.setLevel(logging.INFO)
     filehandler.addFilter(lambda record: record.levelno == logging.INFO)
     # 
-    errorFilehandler = logging.FileHandler(filename=errorFilename)
-    errorFilehandler.setFormatter(formatter)
-    errorFilehandler.setLevel(logging.ERROR)
-    errorFilehandler.addFilter(lambda record: record.levelno == logging.ERROR)
+    if(add_error_log):
+        errorFilehandler = logging.FileHandler(filename=errorFilename)
+        errorFilehandler.setFormatter(formatter)
+        errorFilehandler.setLevel(logging.ERROR)
+        errorFilehandler.addFilter(lambda record: record.levelno == logging.ERROR)
+        logger.addHandler(errorFilehandler)
     # 
     logger.addHandler(filehandler)
     logger.addHandler(consolehanlder)
-    logger.addHandler(errorFilehandler)
     # 
     logger.setLevel(logging.DEBUG)
     # 
