6e460e17c32d394ada9a4e3b05af27ab9fd055cf
ShanelReyes <71140658+ShanelReyes@users.noreply.github.com>
2022-11-09 15:12:37 -0600

    comentarios

--- interfaces/createsecureclusteringworker.py
+++ interfaces/createsecureclusteringworker.py
@@ -1,5 +1,11 @@
 import json
 
+"""
+Description:
+    It is a DTO. Encapsulates features of the worker and manager objects.
+Notes:
+    DTO (Data Transfer Object): Set of attributes that can be sent or retrieved from the server in a single invocation
+"""
 class CreateSecureClusteringWorker(object):
     def __init__(self,**kws):
         self.nodeId    = kws.get("nodeId")
@@ -7,7 +13,6 @@ class CreateSecureClusteringWorker(object):
         self.image     = kws.get("image","secure-clustering-worker:latest")
         self.network   = kws.get("network", {"name":"test","driver":"bridge"})
         self.ports     = kws.get("ports")
-        # 
         default_envs   = {
             "NODE_ID": self.nodeId,
             "NODE_PORT": str(self.ports["docker"]),
@@ -18,7 +23,7 @@ class CreateSecureClusteringWorker(object):
             "LOG_PATH": "/logs",
             "SINK_PATH": "/sink"
         }
-        # _______________
+        
         _envs          = kws.get("envs",{})
         self.envs      = {**default_envs,**_envs}
         self.labels    = kws.get("labels",{})
@@ -27,7 +32,7 @@ class CreateSecureClusteringWorker(object):
             "/test/sink/"+self.nodeId: "/sink"
         })
         self.resources = kws.get("resources",{})
-    # _________________________________________
+        
     def serialize(self):
         return json.dumps(self.__dict__)
 
--- interfaces/secureclusteringmanager.py
+++ interfaces/secureclusteringmanager.py
@@ -1,31 +1,39 @@
 import requests
 
+"""
+Description: 
+    Class that allows the connection between the client and the manager
+Attributes: 
+    hostname, port
+    available ports:
+        Client  -> 3000-5999
+        Manager -> 6000-8999
+        Workers -> 9000+
+"""
 class SecureClusteringManager(object):
     def __init__(self,**kwargs):
-        # _____________________________________________________
         self.hostname      = kwargs.get("hostname","localhost")
         self.port          = kwargs.get("port",6000)
-        # _____________________________________________________
         self.protocol      = kwargs.get("protocol","http")
-        # _____________________________________________________
         self.apiVersion    = kwargs.get("apiVersion",2)
-        # _____________________________________________________
+        
         # <protocol>://<hostname|ip>:<port>
         self.baseUrl       = "{}://{}:{}".format(self.protocol,self.hostname,self.port)
-        # _____________________________________________________
         self.clusteringURL = "{}/clustering/secure".format(self.baseUrl)
-        # _____________________________________________________
     
+    """
+    Description:
+        Function that allows sending the data with the request 
+    Notes:
+        serialize(): convert a value to a sequence of bits to be transmitted across a network.
+    """
     def sendSecureClusteringRequest(self,**kwargs):
         data     = kwargs.get("data")
         _data    = data.serialize()
-        
-        # __________________________________
         response = requests.post(
             self.clusteringURL,
             data = _data,
             headers= {"Content-Type":"application/json"}
         )
-        # _________________________________
         return response
 
